8d7237186b6bc3f7ee27c3a44eda8a69a727acd8
ryananthonydiaz <email@ryananthonydiaz.com>
2020-03-03 23:50:57 -0800

    four helper functions created in users.js to retrieve and remove users from specific chat rooms

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const { generateMessage, generateLocationMessage } = require('./utils/messages');
+const { addUser, removeUser, getUser, getUserInRoom } = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
